70d6d46ce0bd831740d7bfafbe7acbf4cfa12174
Aleqzander Baker <aleqzanderbaker@gmail.com>
2025-10-21 13:04:27 -0600

    fixed cloudinary import error

--- src/server.js
+++ src/server.js
@@ -33,8 +33,8 @@ if (!fs.existsSync(uploadDir)) {
 
 // import session from "express-session";
 
-// import { v2 as cloudinary } from 'cloudinary';
-// import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
 // Configure Cloudinary
 cloudinary.config({
